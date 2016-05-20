2942865dca2ca986bf7f5a3f078ba446668a1273
Khai Phan <khai.phanvan.nt@gmail.com>
2016-05-20 11:38:14 +0700

    Init

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('researchAngularMaterial', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ngMaterial', 'toastr']);
+
+})();
